a7fa345de7486e2fef14722c716a3176c6e4ab62
Amirhossein Souri <amirhsnsouri@gmail.com>
2025-07-24 15:15:04 +0330

    differ selected shape

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,14 +2,22 @@ import type { ShapeType } from "../types/shapes";
 
 const shapes: ShapeType[] = ["circle", "square", "triangle"];
 
-const Sidebar = ({ onSelect }: { onSelect: (s: ShapeType) => void }) => {
+const Sidebar = ({
+  currentShape,
+  onSelect,
+}: {
+  currentShape: ShapeType | null;
+  onSelect: (s: ShapeType) => void;
+}) => {
   return (
     <div className="w-28 p-2 bg-gray-100 space-y-2">
       {shapes.map((s) => (
         <button
           key={s}
           onClick={() => onSelect(s)}
-          className="w-full px-2 py-1 bg-white border rounded hover:bg-gray-200">
+          className={`w-full px-2 py-1 bg-white border rounded hover:bg-gray-200 ${
+            s == currentShape ? "!bg-blue-300" : ""
+          }`}>
           {s}
         </button>
       ))}
